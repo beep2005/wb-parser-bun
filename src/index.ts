bd6d8e77f4d33ac6575985900c5f50ed77326f18
Beep <andrey.dm.agarkov@gmail.com>
2026-07-08 14:16:46 +0300

    drizzle and env setup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+
+const client = postgres(process.env.DATABASE_URL!);
+const db = drizzle(client);
+
 const server = Bun.serve({
-    port: 8000,
+    port: process.env.PORT,
     fetch(req){
         return new Response("hello");
     }
 });
 
 
-console.log("Hello via Bun!");
+console.log(`Hello via Bun! Working on port: ${process.env.PORT}`);
 
