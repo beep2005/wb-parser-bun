69bf15d719cca493fd2d75a4ce1f1cc644c3d742
Beep <andrey.dm.agarkov@gmail.com>
2026-07-31 17:18:42 +0300

    db schema done

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -5,13 +5,14 @@ export const products = pgTable('product', {
     // связь -> один <- ко многим 
     id: integer().primaryKey(),
     name: varchar( {length:255} ).notNull(),
-    price: integer(), // null - если нет в наличии?
-    stock: boolean(),
+    currentPrice: integer(), // null - если нет в наличии?
+    inStock: boolean(),
+    updatedAt: timestamp().defaultNow().notNull(),
 });
 
 export const priceHistory = pgTable('priceHistory', {
     priceAtTime: integer(),
-    actual_at: timestamp().defaultNow(),
+    recordedAt: timestamp().defaultNow(),
     // связь один ко -> многим <-
     productId: integer().notNull().references(() => products.id),
 });
@@ -25,10 +26,10 @@ export const cookiesStorage = pgTable('cookiesStorage', {
 
 // описание связей
 export const productRelations = relations(products, ({ many }) => ({
-    id: many(priceHistory),
+    productHistory: many(priceHistory),
 }));
 export const priceHistoryRelations = relations(priceHistory, ({ one }) => ({
-    id: one(products, {
+    product: one(products, {
         fields: [priceHistory.productId],
         references: [products.id],
     }),
